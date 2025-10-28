2356405bd33ee25e071e753434060fbd3ef6f717
Cornelius Okeke <okekecornelius@gmail.com>
2025-10-28 20:10:54 +0100

    added all templates

---
+++ script.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+const GA_ID = 'G-LT90FFSNPZ';
+const isLocalhost = Boolean(window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.href.startsWith('file://'));	
+
+// Google Analytics(restored custom script)-- >
+// function gtag() { dataLayer.push(arguments); }
+// gtag('js', new Date());
+// gtag('config', GA_ID, {
+// 	page_path: `${window.location.pathname}${window.location.hash || ''}`,
+// 	page_location: window.location.href
+// });
